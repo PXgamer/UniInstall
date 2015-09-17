bae9158c6c732ea56ad281414d9b9fb039e88198
PXgamer <owzie123@gmail.com>
2015-09-17 18:09:45 +0100

    First Push

---
+++ UniInstall - For WinDistro/Loc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniInstall___For_WinDistro
+{
+    class Loc
+    {
+
+        static string atom = @"/Atom/Atom.exe";
+        static string firefox = @"/Firefox/Firefox.exe";
+        static string kmplayer = @"/KMPlayer/KMPlayer.exe";
+        static string lightshot = @"/Lightshot/Lightshot.exe";
+        static string mbae = @"/MalwareBytes Anti-Exploit/MalwareBytes Anti-Exploit.exe";
+        static string mbam = @"/MalwareBytes Anti-Malware/MalwareBytes Anti-Malware.exe";
+        static string mbar = @"/MalwareBytes Anti-Rootkit/";
+        static string photoshop = @"/Photoshop CS6 Extended/SOFTWARE/Set-up.exe";
+        static string pia = @"/Private Internet Access/Private Internet Access.exe";
+        static string sandboxie = @"/Sandboxie/Sandboxie.exe";
+        static string shadow_defender = @"/Shadow Defender/Shadow Defender.exe";
+        static string skype = @"/Skype/Skype.exe";
+        static string vs2013pro = @"/Visual Studio 2013 Pro/Visual Studio 2013 Pro.iso";
+        static string vs2015com = @"/Visual Studio 2015 Community/Visual Studio 2015 Community.exe";
+        static string winrar = @"/WinRAR/WinRAR.exe";
+        static string wisecare365 = @"/WiseCare 365/WiseCare 365.exe";
+
+    }
+}
---
+++ UniInstall - For WinDistro/Start.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniInstall___For_WinDistro
+{
+    public partial class Start : Form
+    {
+        public Start()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 frm = new Form1();
+            frm.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
